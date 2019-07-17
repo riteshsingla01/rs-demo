2fbcf479f72674a5ea72353ee3818692ddf9990e
Ritesh Singla <riteshsingla01@gmail.com>
2019-07-17 13:17:36 -0500

    new service added

--- src/main/java/com/java/demo/DemoApplication.java
+++ src/main/java/com/java/demo/DemoApplication.java
@@ -1,9 +1,9 @@
-package com.java.demo.rsdemo;
+package com.java.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.java.demo"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/java/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.java.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class DemoController {
+	
+	@GetMapping("/validate")
+	public String validate() {
+		
+		return "successfull";
+	}
+
+}
